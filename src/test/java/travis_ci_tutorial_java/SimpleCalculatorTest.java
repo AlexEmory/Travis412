b080bd7227c7c705d8c166ad5b1c54833352615a
Alexandra Emory <aemory1@students.towson.edu>
2019-04-01 09:15:43 -0400

    Added test case for minus, added set up and tear down, and fixed layout of test (its good practice to put expected result first in test)

--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -6,9 +6,23 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 public class SimpleCalculatorTest {
+	SimpleCalculator calc;
+	
+	@Before
+	public void setUp() {
+		calc = new SimpleCalculator();
+	}
+	
 	@Test
 	public void testAdd() {
-		SimpleCalculator calc = new SimpleCalculator();
-		assertEquals(calc.add(1, 1), 2);
+		assertEquals(2,calc.add(1, 1));
+	}
+	@Test
+	public void testMinus() {
+		assertEquals(2, calc.minus(5,3));
+	}
+	@After
+	public void tearDown() {
+		calc=null;
 	}
 }
